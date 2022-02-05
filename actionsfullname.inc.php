29e931d9c04d6b38336a662473fde187b748910c
bvs3040 <taga-si@mail.ru>
2022-02-05 16:21:45 +0700

    getShortName

--- actionsfullname.inc.php
+++ actionsfullname.inc.php
@@ -1,13 +1,40 @@
 <?php
-    function getPartsFromFullname($str){
+
+// функиция разделения ФИО
+
+    function getPartsFromFullname($fullname){
         $partFullname=['surname','name','patronomyc'];
-        $fullnameArray=explode(' ', $str);
+        $fullnameArray=explode(' ', $fullname);
         $partsFromFullname=array_combine($partFullname,$fullnameArray);
         return $partsFromFullname;
     }
+
+// функция склеивания ФИО
            
     function getFullnameFromParts($surname, $name, $patronomyc){
         $fullnameFromParts="$surname" . ' ' . "$name" . ' ' . "$patronomyc";
         return $fullnameFromParts;
     }
+
+// функция Сокращение ФИО
+
+    function getShortName($fullname){
+        $arr=getPartsFromFullname($fullname);
+        $name = $arr['name'];
+        $surname= $arr['surname'];
+        $shortSurname=mb_substr($surname, 0, 1);
+        $shortName="$name" . ' ' . "$shortSurname.";
+        return $shortName;
+    }
+
+    
+
+
+
+
+
+
+
+
+
 ?>        
